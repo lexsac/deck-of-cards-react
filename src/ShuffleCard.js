f860aad34b47aad740ee2e0aaff01979b894ce05
Lexsa Campbell <lexsacampbell@gmail.com>
2023-03-10 13:43:29 -0800

    Added AutoDraw functionality and Restart game button

--- src/ShuffleCard.js
+++ src/ShuffleCard.js
@@ -10,6 +10,9 @@ const ShuffleCard = () => {
     const [drawnCards, setDrawnCards] = useState([]);
     const [numCardsDrawn, setNumCardsDrawn] = useState(0);
 
+    const [autoDraw, setAutoDraw] = useState(false);
+    const timerId = useRef(null);
+
     useEffect(() => {
         async function getDeckId() {
             let d = await axios.get(`${API_BASE_URL}/new/shuffle`);
@@ -19,35 +22,57 @@ const ShuffleCard = () => {
         getDeckId();
     }, []);
 
-    useEffect(() => {
-        async function drawCard() {
-            if (numCardsDrawn === 52) {
-                alert('Error: no cards remaining!');
-            } else {
-                let cardData = await axios.get(`${API_BASE_URL}/${deckIdRef.current}/draw`);
-            
-                let card = cardData.data.cards[0];
-    
-                setDrawnCards(d => [
-                    ...d,
-                    {
-                        id: card.code,
-                        name: card.value + ' of ' + card.suit,
-                        image_url: card.image
-                    }
-                ]);
-            }
+    const drawCard = async () => {
+        if (numCardsDrawn === 52) {
+            setAutoDraw(false);
+            alert('Error: no cards remaining!');
+        } else {
+            let cardData = await axios.get(`${API_BASE_URL}/${deckIdRef.current}/draw`);
+            let card = cardData.data.cards[0];
+
+            setDrawnCards(d => [
+                ...d,
+                {
+                    id: card.code,
+                    name: card.value + ' of ' + card.suit,
+                    image_url: card.image
+                }
+            ]);
+            setNumCardsDrawn(numCardsDrawn + 1);
         }
-        drawCard();
-    }, [numCardsDrawn])
+    }
+
+    useEffect(() => {
+        if (autoDraw) {
+            timerId.current = setInterval(() => {
+                drawCard();
+            }, 1000);
+        } else {
+            clearInterval(timerId.current);
+        }
+        return () => clearInterval(timerId.current);
+    }, [autoDraw, numCardsDrawn])
+      
+    const toggleAutoDraw = () => {
+        setAutoDraw(autoDraw => !autoDraw);
+    }
 
     const cards = drawnCards.map(c => (
         <Card key={c.id} name={c.name} image={c.image_url} />
     ));
 
+    const restartGame = () => {
+        setDrawnCards([]);
+        setNumCardsDrawn(0);
+        setAutoDraw(false);
+    }
+
     return (
         <div>
-            <button onClick={() => setNumCardsDrawn(numCardsDrawn + 1)}>Draw a Card</button>
+            <button onClick={drawCard}>Draw a card</button>
+            <button onClick={toggleAutoDraw}>{(autoDraw) ? 'Stop drawing' : 'Start drawing'}</button>
+            <button onClick={restartGame}>Restart game</button>
+            <p>Number of cards drawn: {numCardsDrawn}</p>
             {cards}
         </div>
     );
